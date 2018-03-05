b9421fade56b5c50c93e7bba0056d9222c4b05ad
jimdog6125 <24468320+jimdog6125@users.noreply.github.com>
2018-03-05 17:40:30 -0600

    Update main.js

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ function buySword(){
         document.getElementById('swords').innerHTML = swords;  //updates the number of cursors for the user
         document.getElementById('kills').innerHTML = kills;  //updates the number of cookies for the user
     };
-    var nextCost = Math.floor(10 * Math.pow(1.1,swords));       //works out the cost of the next cursor
+    var nextCost = Math.floor(20 * Math.pow(1.1,swords));       //works out the cost of the next cursor
     document.getElementById('swordCost').innerHTML = nextCost;  //updates the cursor cost for the user
 };
 
