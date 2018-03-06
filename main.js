5cbf0168a2cbd602123be955394195e10053d40b
jimdog6125 <24468320+jimdog6125@users.noreply.github.com>
2018-03-06 17:16:30 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
-var cookies = 0;
+var kills = 0;
 
 function cookieClick(number){
-    cookies = cookies + number;
-    document.getElementById("cookies").innerHTML = cookies;
+    kills = kills + number;
+    document.getElementById("kills").innerHTML = kills;
 };
 
-var cursors = 0;
+var swords = 0;
 
 function buyCursor(){
-    var cursorCost = Math.floor(10 * Math.pow(1.1,cursors));     //works out the cost of this cursor
-    if(cookies >= cursorCost){                                   //checks that the player can afford the cursor
-        cursors = cursors + 1;                                   //increases number of cursors
-    	cookies = cookies - cursorCost;                          //removes the cookies spent
-        document.getElementById('cursors').innerHTML = cursors;  //updates the number of cursors for the user
-        document.getElementById('cookies').innerHTML = cookies;  //updates the number of cookies for the user
+    var swordCost = Math.floor(10 * Math.pow(1.1,swords));     //works out the cost of this cursor
+    if(kills >= swordCost){                                   //checks that the player can afford the cursor
+        swords = swords + 1;                                   //increases number of cursors
+    	kills = kills - swordCost;                          //removes the cookies spent
+        document.getElementById('swords').innerHTML = swords;  //updates the number of cursors for the user
+        document.getElementById('kills').innerHTML = kills;  //updates the number of cookies for the user
     };
-    var nextCost = Math.floor(10 * Math.pow(1.1,cursors));       //works out the cost of the next cursor
-    document.getElementById('cursorCost').innerHTML = nextCost;  //updates the cursor cost for the user
+    var nextCost = Math.floor(10 * Math.pow(1.1,swords));       //works out the cost of the next cursor
+    document.getElementById('swordCost').innerHTML = nextCost;  //updates the cursor cost for the user
 };
 
 var magic = 0;
@@ -29,7 +29,7 @@ function buyMagic(){
         document.getElementById('magic').innerHTML = magic;  //updates the number of cursors for the user
         document.getElementById('cookies').innerHTML = cookies;  //updates the number of cookies for the user
     };
-    var nextCost = Math.floor(10 * Math.pow(1.1,magic));       //works out the cost of the next cursor
+    var nextCost = Math.floor(25 * Math.pow(1.1,magic));       //works out the cost of the next cursor
     document.getElementById('magicCost').innerHTML = nextCost;  //updates the cursor cost for the user
 };
 window.setInterval(function(){
@@ -41,5 +41,5 @@ window.setInterval(function(){
 	
 	cookieClick(magic);
 
-}, 200);
+}, 400);
 
