4f75dfc1002553fdee13b2c612bef8241220a0b4
jimdog6125 <24468320+jimdog6125@users.noreply.github.com>
2018-03-06 14:44:00 -0600

    Update main.js

--- main.js
+++ main.js
@@ -19,8 +19,21 @@ function buyCursor(){
     document.getElementById('cursorCost').innerHTML = nextCost;  //updates the cursor cost for the user
 };
 
+var magic = 0;
+
+function buyMagic(){
+    var magicCost = Math.floor(10 * Math.pow(1.1,magic));     //works out the cost of this cursor
+    if(cookies >= magicCost){                                   //checks that the player can afford the cursor
+        magic = magic + 1;                                   //increases number of cursors
+    	cookies = cookies - magicCost;                          //removes the cookies spent
+        document.getElementById('magic').innerHTML = magic;  //updates the number of cursors for the user
+        document.getElementById('cookies').innerHTML = cookies;  //updates the number of cookies for the user
+    };
+    var nextCost = Math.floor(10 * Math.pow(1.1,magic));       //works out the cost of the next cursor
+    document.getElementById('magicCost').innerHTML = nextCost;  //updates the cursor cost for the user
+};
 window.setInterval(function(){
-	
+	cookieClick(magic);
 	cookieClick(cursors);
 	
 }, 1000);
