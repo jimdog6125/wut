915ea89d4169e4e009223ec83c693ce34d4566a0
jimdog6125 <24468320+jimdog6125@users.noreply.github.com>
2018-03-06 20:37:51 -0600

    Update main.js

--- main.js
+++ main.js
@@ -23,22 +23,22 @@ var magic = 0;
 
 function buyMagic(){
     var magicCost = Math.floor(25 * Math.pow(1.1,magic));     //works out the cost of this cursor
-    if(cookies >= magicCost){                                   //checks that the player can afford the cursor
+    if(kills >= magicCost){                                   //checks that the player can afford the cursor
         magic = magic + 1;                                   //increases number of cursors
-    	cookies = cookies - magicCost;                          //removes the cookies spent
+    	kills = kills - magicCost;                          //removes the cookies spent
         document.getElementById('magic').innerHTML = magic;  //updates the number of cursors for the user
-        document.getElementById('cookies').innerHTML = cookies;  //updates the number of cookies for the user
+        document.getElementById('kills').innerHTML = kills;  //updates the number of cookies for the user
     };
     var nextCost = Math.floor(25 * Math.pow(1.1,magic));       //works out the cost of the next cursor
     document.getElementById('magicCost').innerHTML = nextCost;  //updates the cursor cost for the user
 };
 window.setInterval(function(){
 	
-	cookieClick(cursors);
+	kill(swords);
 	
 }, 1000);
 window.setInterval(function(){
 	
-	cookieClick(magic);
+	kill(magic);
 
 }, 400);
