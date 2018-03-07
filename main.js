0a75e4134edd223017f2b714a9d6da2413d2d6e9
jimdog6125 <24468320+jimdog6125@users.noreply.github.com>
2018-03-06 21:27:17 -0600

    Update main.js

--- main.js
+++ main.js
@@ -22,15 +22,28 @@ function buySword(){
 var magic = 0;
 
 function buyMagic(){
-    var magicCost = Math.floor(25 * Math.pow(1.1,magic));     //works out the cost of this cursor
+    var magicCost = Math.floor(100 * Math.pow(1.1,magic));     //works out the cost of this cursor
     if(kills >= magicCost){                                   //checks that the player can afford the cursor
         magic = magic + 1;                                   //increases number of cursors
     	kills = kills - magicCost;                          //removes the cookies spent
         document.getElementById('magic').innerHTML = magic;  //updates the number of cursors for the user
         document.getElementById('kills').innerHTML = kills;  //updates the number of cookies for the user
     };
-    var nextCost = Math.floor(25 * Math.pow(1.1,magic));       //works out the cost of the next cursor
+    var nextCost = Math.floor(100 * Math.pow(1.1,magic));       //works out the cost of the next cursor
     document.getElementById('magicCost').innerHTML = nextCost;  //updates the cursor cost for the user
+};
+var cannons = 0;
+
+function buyCannon(){
+    var cannonCost = Math.floor(300 * Math.pow(1.1,cannon));     //works out the cost of this cursor
+    if(kills >= cannonCost){                                   //checks that the player can afford the cursor
+        cannons = cannons + 1;                                   //increases number of cursors
+    	kills = kills - cannonCost;                          //removes the cookies spent
+        document.getElementById('cannon').innerHTML = cannons;  //updates the number of cursors for the user
+        document.getElementById('kills').innerHTML = kills;  //updates the number of cookies for the user
+    };
+    var nextCost = Math.floor(300 * Math.pow(1.1,cannons));       //works out the cost of the next cursor
+    document.getElementById('cannonCost').innerHTML = nextCost;  //updates the cursor cost for the user
 };
 window.setInterval(function(){
 	
@@ -42,3 +55,8 @@ window.setInterval(function(){
 	kill(magic);
 
 }, 1000);
+window.setInterval(function(){
+	
+	kill(cannons);
+
+}, 500);
